50cddf7d6f0a4230182029f75ecf1541274ec95c
aguirguis <30687406+aguirguis@users.noreply.github.com>
2018-10-10 13:23:20 +0200

    Update perfect_link.cpp

--- perfect_link.cpp
+++ perfect_link.cpp
@@ -1,9 +1,10 @@
 #include <string>
+#include "structures.h"
 using namespace std;
 
 class perfect_link {
 
     public:
-        void send(string message, int to) {}
-        void deliver(string message, int from) {}
+        void send(Message message, int to) {}
+        void deliver(Message message, int from) {}
 };
