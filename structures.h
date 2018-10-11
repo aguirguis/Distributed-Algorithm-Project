41a169027c39dd64d7acd7c9a0032ccd50195ee1
aguirguis <30687406+aguirguis@users.noreply.github.com>
2018-10-11 10:12:11 +0200

    Update structures.h

--- structures.h
+++ structures.h
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+#define MAX_PROCESSES_NUM 10
+#define MSG_LEN 32
+#define MAX_LOG_FILE 100	//let's say after each 100 message, I will write to a file
+
+int nb_of_processes;
+Process processes[MAX_PROCESSES_NUM];
+
+int process_id;
+string my_ip;
+int my_port;
+int sock_client, sock_server;
+LogMessage log[MAX_LOG_FILE];
+int log_pointer = 0;
+
 struct Message{
   int sender;
   int seq_no;
