161d1d8c1efcc97e8e98f93fc238572a04c012af
aguirguis <30687406+aguirguis@users.noreply.github.com>
2018-10-15 14:59:12 +0200

    Update structures.h

--- structures.h
+++ structures.h
@@ -7,7 +7,8 @@ using namespace std;
 #define MAX_LOG_FILE 100	//let's say after each 100 message, I will write to a file
 
 struct Message{
-  int sender;
+  int initial_sender;
+  int direct_sender;
   int seq_no;
 };
 
@@ -32,3 +33,19 @@ int my_port;
 int sock_client, sock_server;
 LogMessage log[MAX_LOG_FILE];
 int log_pointer = 0;
+ofstream out_file;
+
+//This function writes the logs received to the log file...it takes proc_id as an input
+static void write_log(){
+	for(int i = 0; i < log_pointer; i++) {
+		//write processes[proc_id].log[i] to file
+		if(log[i].message_type == 'b') {
+			out_file << "b " << log[i].seq_nr << "\n";
+		}
+		else {
+			out_file << "d " << log[i].sender << " " << log[i].seq_nr << "\n";
+		}
+	}
+	out_file.close();
+	log_pointer = 0;	//return the point to the beginning
+}
