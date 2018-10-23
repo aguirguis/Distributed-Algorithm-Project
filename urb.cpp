b989dcbfd3f5ed861cf10aa8f7833146a67363df
CarlHenrikRolf <henrikaslund@eduroam-2-231.epfl.ch>
2018-10-23 08:48:55 +0200

    urb added

---
+++ urb.cpp
@@ -0,0 +1,121 @@
+//
+//  urb.cpp
+//  project1
+//
+//  Created by Henrik Åslund on 2018-10-14.
+//  Copyright © 2018 Henrik Åslund. All rights reserved.
+//
+
+#include "structures.h"
+#include "beb.h"
+#include "urb.hpp"
+//#include "beb.h"
+
+void urb::urbBroadcast(Message message) {
+    bbb.init(this);
+    // add itself as the immediate sender
+    // and put into the pending set
+    message.sender = my_process_id;
+    pending.insert(message);
+    // since we did insert something in pending here,
+    // we should probably check the condition
+    // upon exists
+    while(it != pending.end()) {
+        if(candeliver(*it) && (delivered.find(*it) == delivered.end())) {
+            delivered.insert(*it);
+            urb_deliver(*it, (*it).sender, ack);
+        }
+        it++;
+    }
+    // Broadcast
+    bbb.bebBroadcast(message);
+}
+
+void urb::urb_deliver(Message message, int from, set<int, greater <int> > ack[]) {
+    // add process to acknowledgement array
+    // here, we identify messages by their initial sender
+    // this may not be entirely correct,
+    // since the initial sender could send several messages.
+    // however, we leave this for another level of abstraction
+    int i = 0;
+    while(true) {
+        if(messageInd[i].initial_sender == message.initial_sender) {
+            ack[i].insert(from);
+            i = 0;
+            break;
+        }
+        else if(i<=nMessages) {
+            i++;
+        }
+        else {
+            messageInd[i] = message;
+            nMessages++;
+            ack[i].insert(from);
+            i = 0;
+            break;
+        }
+    }
+    // check if pending
+    // here again, it's not entirely clear
+    // how to identify messages.
+    // this is wrong?!
+    bool notInPending = true;
+    while(it != pending.end()) {
+        if((*it).initial_sender == message.initial_sender
+           /*&& (*it).seq_no == message.seq_no*/) {
+            notInPending = false;
+            break;
+        }
+        else {
+            it++;
+        }
+    }
+    if(notInPending) {
+        pending.insert(message);
+        bbb.bebBroadcast(message);
+    }
+}
+
+void urb::deliver(Message message) {
+    
+    urb_deliver(message, message.sender, ack);
+    
+    // upon exists
+    while(it != pending.end()) {
+        if(candeliver(*it) && (delivered.find(*it) == delivered.end())) {
+            delivered.insert(*it);
+            bbb.beb_deliver(*it, int ((*it).sender));
+        }
+        it++;
+    }
+}
+
+
+
+bool urb::candeliver(Message message) {
+    // calculate the number of acks for this message
+    int nAcks = 0;
+    int i = 0;
+    while(true) {
+        if(messageInd[i].seq_no == message.seq_no
+           && messageInd[i].sender == message.sender
+           && messageInd[i].initial_sender == message.initial_sender) {
+            nAcks = ack[i].size();
+            i = 0;
+            break;
+        }
+        else if(i <= nMessages) {
+            i++;
+        }
+        else {
+            i = 0;
+            break;
+        }
+    }
+    // return statement whether majority or not
+    return nAcks > nb_of_processes/2;
+}
+
+
+
+
---
+++ urb.hpp
@@ -0,0 +1,39 @@
+//
+//  urb.hpp
+//  project1
+//
+//  Created by Henrik Åslund on 2018-10-14.
+//  Copyright © 2018 Henrik Åslund. All rights reserved.
+//
+
+#ifndef urb_hpp
+#define urb_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <set>
+#include <iterator>
+#include "beb.h"
+#include "structures.h"
+
+using namespace std;
+
+class urb: public deliver_callback {
+private:
+    set<Message, MessageComp> delivered;
+    set<Message, MessageComp> pending;
+    set<Message, MessageComp>::iterator it = pending.begin();
+    int nMessages = 0;
+    Message messageInd[0];
+    set<int, greater <int> > ack[0];
+    beb bbb;
+    //Process self;
+    
+public:
+    bool candeliver(Message message);
+    void urbBroadcast(Message message);
+    void deliver(Message message);
+    void urb_deliver(Message message, int from, set<int, greater <int> > ack[]);
+};
+
+#endif /* urb_hpp */
